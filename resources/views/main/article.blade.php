0dabeb9a6d20f2fc6b418a46ef2a87dd8344f58a
Maksim <maksimkaklass2006@gmail.com>
2025-09-10 11:14:07 +0300

    Lab2 done

---
+++ resources/views/main/article.blade.php
@@ -0,0 +1,25 @@
+@extends('layout')
+@section('content')
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">date</th>
+      <th scope="col">name</th>
+      <th scope="col">shortDesc</th>
+      <th scope="col">desc</th>
+      <th scope="col">preview image</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($articles as $article)
+    <tr>
+      <th scope="row">{{$article->date}}</th>
+      <td>{{$article->name}}</td>
+      <td>{{$article->shortDesc}}</td>
+      <td>{{$article->desc}}</td>
+      <td><a href="/full_image/{{$article->full_image}}"><img src="{{URL::asset($article->preview_image)}}" alt="image"></a></td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MainController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layout');
-});
+Route::get('/', [MainController::class, 'index']);
+Route::get('/full_image/{img}', [MainController::class, 'show']);
+
 
 Route::get('/about', function () {
     return view('main.about');
